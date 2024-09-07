16c2740e049b63856985809900ebd373cc029253
Adam Neighbors <onelegchair@gmail.com>
2024-09-07 11:42:03 -0500

    Update - config file cleanup

--- config.py
+++ config.py
@@ -3,12 +3,19 @@ from pathlib import Path
 
 
 class ConfigFile(RawConfigParser):
-    def __init__(self, config_file: str = 'config.ini'):
+    """Loads given config file.
+
+    Args:
+        config_file_location (str, optional): Location of config file. Defaults to 'config.ini'.
+
+    Raises:
+        FileNotFoundError: Error if config file doesn't exist.
+    """
+    def __init__(self, config_file_location: str = 'config.ini'):
         super().__init__()
-        if not Path(config_file).is_file():
-            print('config.ini file doesn\'t exist')
-            return False
-        self.read(config_file)
+        if not Path(config_file_location).is_file():
+            raise FileNotFoundError(f"the file {config_file_location} doesn't exist.")
+        self.read(config_file_location)
 
 
 config_file = ConfigFile()
